854473660c83d6e86729381b872e73f18b0fd766
Sławomir Lasik <--global>
2020-05-03 19:15:11 +0200

    Resolved issue with feeding validation with whitespaces usint @InitBinder method that trims all strings before validation also setting to null strings with all whitespaces. It uses StringTrimmerEditor class from org.springframework.beans.propertyeditors.StringTrimmerEditor package

--- src/com/slawomirlasik/springdemo/mvc/CustomerController.java
+++ src/com/slawomirlasik/springdemo/mvc/CustomerController.java
@@ -2,15 +2,30 @@ package com.slawomirlasik.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+	// add an initbinder .. to convert trim input strings
+	// remove leading and trailing whitespace
+	// resolve issu for our validation
+
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) {
+
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+		
+		dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+	}
 
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
@@ -21,6 +36,8 @@ public class CustomerController {
 
 	@RequestMapping("/processForm")
 	public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult theBindingResult) {
+
+		System.out.println("Last name: |" + theCustomer.getLastName() + "|");
 
 		if (theBindingResult.hasErrors()) {
 			return "customer-form";
