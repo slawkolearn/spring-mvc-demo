3e10808105373f951525cd5bd42db5ff858caed6
Sławomir Lasik <--global>
2020-05-04 16:50:47 +0200

    made freePasses field in Customer class required. Conversion error though when trying to put to validate empty field

--- src/com/slawomirlasik/springdemo/mvc/Customer.java
+++ src/com/slawomirlasik/springdemo/mvc/Customer.java
@@ -13,7 +13,8 @@ public class Customer {
 	@NotNull(message="is required")
 	@Size(min=1, message="is required")
 	private String lastName;
-	
+
+	@NotNull(message="is required")
 	@Min(value=0, message="must be greater than or equal to zero")
 	@Max(value=10, message="must be less than or equal to ten")
 	private int freePasses;
